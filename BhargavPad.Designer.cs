7c418ffb4f6a176340c2488a3b362fe31acb7f5e
bkpat <bkpat@DESKTOP-534ESRK>
2022-04-04 19:46:07 -0400

    Add project files.

---
+++ BhargavPad.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace BhargavPad
+{
+    partial class formBhargavPad
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formBhargavPad));
+            this.menuTop = new System.Windows.Forms.MenuStrip();
+            this.menuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFileNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFIleSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFIleSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEditCut = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEditCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEditPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEditSelectAll = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuHelpAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxBody = new System.Windows.Forms.TextBox();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.menuTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuTop
+            // 
+            this.menuTop.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.menuTop.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFile,
+            this.menuEdit,
+            this.menuHelp});
+            this.menuTop.Location = new System.Drawing.Point(0, 0);
+            this.menuTop.Name = "menuTop";
+            this.menuTop.Size = new System.Drawing.Size(1566, 52);
+            this.menuTop.TabIndex = 0;
+            this.menuTop.Text = "menuStrip1";
+            // 
+            // menuFile
+            // 
+            this.menuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFileNew,
+            this.menuFileOpen,
+            this.menuFIleSave,
+            this.menuFIleSaveAs,
+            this.menuFileExit});
+            this.menuFile.Name = "menuFile";
+            this.menuFile.Size = new System.Drawing.Size(87, 48);
+            this.menuFile.Text = "&File";
+            // 
+            // menuFileNew
+            // 
+            this.menuFileNew.Image = ((System.Drawing.Image)(resources.GetObject("menuFileNew.Image")));
+            this.menuFileNew.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuFileNew.Name = "menuFileNew";
+            this.menuFileNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.menuFileNew.Size = new System.Drawing.Size(366, 54);
+            this.menuFileNew.Text = "&New";
+            this.menuFileNew.ToolTipText = "create new file";
+            this.menuFileNew.Click += new System.EventHandler(this.NewClick);
+            // 
+            // menuFileOpen
+            // 
+            this.menuFileOpen.Image = ((System.Drawing.Image)(resources.GetObject("menuFileOpen.Image")));
+            this.menuFileOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuFileOpen.Name = "menuFileOpen";
+            this.menuFileOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.menuFileOpen.Size = new System.Drawing.Size(366, 54);
+            this.menuFileOpen.Text = "&Open";
+            this.menuFileOpen.ToolTipText = "Open the file from the desktop";
+            // 
+            // menuFIleSave
+            // 
+            this.menuFIleSave.Image = ((System.Drawing.Image)(resources.GetObject("menuFIleSave.Image")));
+            this.menuFIleSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuFIleSave.Name = "menuFIleSave";
+            this.menuFIleSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.menuFIleSave.Size = new System.Drawing.Size(366, 54);
+            this.menuFIleSave.Text = "&Save";
+            this.menuFIleSave.ToolTipText = "Save the written text";
+            this.menuFIleSave.Click += new System.EventHandler(this.SaveClick);
+            // 
+            // menuFIleSaveAs
+            // 
+            this.menuFIleSaveAs.Name = "menuFIleSaveAs";
+            this.menuFIleSaveAs.Size = new System.Drawing.Size(366, 54);
+            this.menuFIleSaveAs.Text = "Save &As";
+            this.menuFIleSaveAs.ToolTipText = "Save the file if it\'s new or save same file with different name";
+            this.menuFIleSaveAs.Click += new System.EventHandler(this.SaveAsClick);
+            // 
+            // menuFileExit
+            // 
+            this.menuFileExit.Name = "menuFileExit";
+            this.menuFileExit.Size = new System.Drawing.Size(366, 54);
+            this.menuFileExit.Text = "E&xit";
+            this.menuFileExit.ToolTipText = "Exit the application";
+            this.menuFileExit.Click += new System.EventHandler(this.ExitClick);
+            // 
+            // menuEdit
+            // 
+            this.menuEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuEditCut,
+            this.menuEditCopy,
+            this.menuEditPaste,
+            this.menuEditSelectAll});
+            this.menuEdit.Name = "menuEdit";
+            this.menuEdit.Size = new System.Drawing.Size(92, 48);
+            this.menuEdit.Text = "&Edit";
+            // 
+            // menuEditCut
+            // 
+            this.menuEditCut.Image = ((System.Drawing.Image)(resources.GetObject("menuEditCut.Image")));
+            this.menuEditCut.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuEditCut.Name = "menuEditCut";
+            this.menuEditCut.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.menuEditCut.Size = new System.Drawing.Size(358, 54);
+            this.menuEditCut.Text = "Cu&t";
+            this.menuEditCut.ToolTipText = "Cut the selected text";
+            this.menuEditCut.Click += new System.EventHandler(this.CutClick);
+            // 
+            // menuEditCopy
+            // 
+            this.menuEditCopy.Image = ((System.Drawing.Image)(resources.GetObject("menuEditCopy.Image")));
+            this.menuEditCopy.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuEditCopy.Name = "menuEditCopy";
+            this.menuEditCopy.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.menuEditCopy.Size = new System.Drawing.Size(358, 54);
+            this.menuEditCopy.Text = "&Copy";
+            this.menuEditCopy.ToolTipText = "Copy the selected text";
+            this.menuEditCopy.Click += new System.EventHandler(this.CopyClick);
+            // 
+            // menuEditPaste
+            // 
+            this.menuEditPaste.Image = ((System.Drawing.Image)(resources.GetObject("menuEditPaste.Image")));
+            this.menuEditPaste.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuEditPaste.Name = "menuEditPaste";
+            this.menuEditPaste.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.menuEditPaste.Size = new System.Drawing.Size(358, 54);
+            this.menuEditPaste.Text = "&Paste";
+            this.menuEditPaste.ToolTipText = "Paste the cut or copied text";
+            this.menuEditPaste.Click += new System.EventHandler(this.PasteClick);
+            // 
+            // menuEditSelectAll
+            // 
+            this.menuEditSelectAll.Name = "menuEditSelectAll";
+            this.menuEditSelectAll.Size = new System.Drawing.Size(358, 54);
+            this.menuEditSelectAll.Text = "Se&lect All";
+            this.menuEditSelectAll.ToolTipText = "Select all text";
+            this.menuEditSelectAll.Click += new System.EventHandler(this.SelectAllClick);
+            // 
+            // menuHelp
+            // 
+            this.menuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator5,
+            this.menuHelpAbout});
+            this.menuHelp.Name = "menuHelp";
+            this.menuHelp.Size = new System.Drawing.Size(104, 48);
+            this.menuHelp.Text = "&Help";
+            // 
+            // toolStripSeparator5
+            // 
+            this.toolStripSeparator5.Name = "toolStripSeparator5";
+            this.toolStripSeparator5.Size = new System.Drawing.Size(445, 6);
+            // 
+            // menuHelpAbout
+            // 
+            this.menuHelpAbout.Name = "menuHelpAbout";
+            this.menuHelpAbout.Size = new System.Drawing.Size(448, 54);
+            this.menuHelpAbout.Text = "&About...";
+            this.menuHelpAbout.ToolTipText = "About will provide information about application";
+            this.menuHelpAbout.Click += new System.EventHandler(this.menuHelpAbout_Click);
+            // 
+            // textBoxBody
+            // 
+            this.textBoxBody.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxBody.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxBody.Location = new System.Drawing.Point(0, 52);
+            this.textBoxBody.Multiline = true;
+            this.textBoxBody.Name = "textBoxBody";
+            this.textBoxBody.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxBody.Size = new System.Drawing.Size(1566, 895);
+            this.textBoxBody.TabIndex = 1;
+            this.toolTip.SetToolTip(this.textBoxBody, "Write the text here");
+            // 
+            // formBhargavPad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1566, 947);
+            this.Controls.Add(this.textBoxBody);
+            this.Controls.Add(this.menuTop);
+            this.MainMenuStrip = this.menuTop;
+            this.Name = "formBhargavPad";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "KylePad";
+            this.menuTop.ResumeLayout(false);
+            this.menuTop.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuTop;
+        private System.Windows.Forms.TextBox textBoxBody;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.ToolStripMenuItem menuFile;
+        private System.Windows.Forms.ToolStripMenuItem menuFileNew;
+        private System.Windows.Forms.ToolStripMenuItem menuFileOpen;
+        private System.Windows.Forms.ToolStripMenuItem menuFIleSave;
+        private System.Windows.Forms.ToolStripMenuItem menuFIleSaveAs;
+        private System.Windows.Forms.ToolStripMenuItem menuFileExit;
+        private System.Windows.Forms.ToolStripMenuItem menuEdit;
+        private System.Windows.Forms.ToolStripMenuItem menuEditCut;
+        private System.Windows.Forms.ToolStripMenuItem menuEditCopy;
+        private System.Windows.Forms.ToolStripMenuItem menuEditPaste;
+        private System.Windows.Forms.ToolStripMenuItem menuEditSelectAll;
+        private System.Windows.Forms.ToolStripMenuItem menuHelp;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
+        private System.Windows.Forms.ToolStripMenuItem menuHelpAbout;
+    }
+}
+
---
+++ BhargavPad.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BhargavPad
+{
+    public partial class formBhargavPad : Form
+    {
+
+        string openFile = String.Empty;
+
+        public formBhargavPad()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This will close the form.
+        /// </summary>
+        private void ExitClick(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// This will save the file.
+        /// </summary>
+        private void SaveClick(object sender, EventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// This will cut the selected properties. 
+        /// </summary>
+        private void CutClick(object sender, EventArgs e)
+        {
+            textBoxBody.Cut();
+        }
+
+        /// <summary>
+        /// This will copy the selected properties.
+        /// </summary>
+        private void CopyClick(object sender, EventArgs e)
+        {
+            textBoxBody.Copy();
+        }
+
+        /// <summary>
+        /// This will paste the cut or copied text. 
+        /// </summary>
+        
+        private void PasteClick(object sender, EventArgs e)
+        {
+            textBoxBody.Paste();
+        }
+
+        /// <summary>
+        /// This will select the all text that are in the textbox.
+        /// </summary>
+        private void SelectAllClick(object sender, EventArgs e)
+        {
+            textBoxBody.SelectAll();
+        }
+
+        private void NewClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveAsClick(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "Text files (*.txt)|*.txt";
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                openFile = saveDialog.FileName;
+
+                SaveFile(openFile);
+            }
+
+        }
+  
+        /// <summary>
+        ///  This will save the file at the path indicated by the filename passed in.
+        /// </summary>
+        /// <param name="fileName">It's file name!</param>
+        private void SaveFile(string fileName)
+        {
+            FileStream fileToAccess = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(fileToAccess);
+
+            writer.Write(textBoxBody.Text);
+
+            writer.Close();
+
+        }
+
+        private void menuHelpAbout_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
